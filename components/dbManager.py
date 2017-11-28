03c4abf278fd9cf9e4d004dfeac7521f1fd38fd9
Rafsan Ratul <ratulrafsan@gmail.com>
2017-11-28 09:20:27 +0600

    Bug Fix: Failed to insert new column for auto-incrementing and not-null fields

--- components/dbManager.py
+++ components/dbManager.py
@@ -34,10 +34,6 @@ class DBManager(QObject):
     def executor(self, sql):
         try:
             self.cursor.execute(sql)
-            if self.cursor.description is not None:
-                self.isTable = True
-            else:
-                self.isTable = False
             self.sqlExecuted.emit()
             print("executed: "+sql)
             self.setStatusBarMessage("executed: " + sql)
@@ -110,15 +106,18 @@ class DBManager(QObject):
         self.executor(sql)
 
     def hasAutoIncColumn(self, tableName):
-        sql = "select 1 from sqlite_master where tbl_name='{}' and sql like '%AUTOINCREMENT%';".format(tableName)
-        resp = self.executor(sql)
-        if resp is False:
-            return None
-        else:
-            data = self.cursor.fetchall()
+        sql = "select 1 from sqlite_master where tbl_name='{}' and sql like '%AUTOINCREMENT%' or '%autoincrement%'".format(tableName)
+        try:
+            resp = self.cursor.execute(sql)
+            data = resp.fetchall()
             if len(data) > 0:
                 return data[0][0]
             return 0
+        except Exception as e:
+            print(e)
+            self.error = e
+            self.setStatusBarMessage("Error: "+ str(e))
+            return None
 
     def getAutoIncColumnNames(self, tableName):
         resp = self.hasAutoIncColumn(tableName)
@@ -127,40 +126,46 @@ class DBManager(QObject):
 
         names = []
         sql = "select sql from sqlite_master where tbl_name='{}'".format(tableName)
-        resp = self.executor(sql)
-        if resp is False:
-            return []
+        try:
 
-        colNames = self.getColumnNames(tableName)
-        schema = self.cursor.fetchall()
-        if len(schema) > 0:
-            schema = schema[0][0].split(',')
-        candidates = []
-        for col in schema:
-            if 'autoincrement' in col.lower():
-                candidates.append(col)
-        # too tired to hack-up a generator
-        # TODO: use generator?
-        if len(candidates) > 0:
-            for d in candidates:
-                for col in colNames:
-                    if col in d:
-                        names.append(col)
-
-        return names
+            resp = self.cursor.execute(sql)
+            schema = resp.fetchall()
+            colNames = self.getColumnNames(tableName)
+            print(schema)
+            if len(schema) > 0:
+                schema = schema[0][0].split(',')
+            candidates = []
+            for col in schema:
+                if 'autoincrement' in col.lower():
+                    candidates.append(col)
+            # too tired to hack-up a generator
+            # TODO: use generator?
+            if len(candidates) > 0:
+                for d in candidates:
+                    for col in colNames:
+                        if col in d:
+                            names.append(col)
+            return names
+        except Exception as e:
+            self.error = e
+            self.setStatusBarMessage("Error: "+ str(e))
+            return names
 
     def getNotNullColNames(self, tableName):
         names = []
         sql = "pragma table_info({})".format(tableName)
-        res = self.executor(sql)
-        if res is False:
-            return []
-        data = self.cursor.fetchall()
-        if len(data) > 0:
-            for dataSet in data:
-                if str(dataSet[3]) == "1":
-                    names.append(dataSet[1])
-        return names
+        try:
+            res = self.cursor.execute(sql)
+            data = res.fetchall()
+            if len(data) > 0:
+                for dataSet in data:
+                    if str(dataSet[3]) == "1":
+                        names.append(dataSet[1])
+            return names
+        except Exception as e:
+            self.error = e
+            print("Error :" + str(e))
+            self.setStatusBarMessage("Error: "+ str(e))
 
     def addNewRow(self, tableName, colNames, values):
         sql = "insert into "+ tableName + " " + str(tuple(colNames)) + " values ("
@@ -170,11 +175,17 @@ class DBManager(QObject):
 
     def addEmptyRow(self, tableName):
         notNull = self.getNotNullColNames(tableName)
+        print("[INFO: AER] notNullCols" + str(notNull))
         if len(notNull) == 0:
             colNames = self.getColumnNames(tableName)
-            notNull = list(set(colNames) - set(self.getAutoIncColumnNames(tableName)))
+            print("[INFO: AER] colNames" + str(colNames))
+            aiColNames = self.getAutoIncColumnNames(tableName)
+            print("[INFO: AER] aiColNames" + str(aiColNames))
+            notNull = list(set(colNames) - set(aiColNames))
+            print("[INFO: AER] new notNull" + str(notNull))
             if len(notNull) == 0:
                 notNull = colNames
+                print("[INFO: AER] else notNull" + str(notNull))
         sql = "insert into " + tableName + " " + str(tuple(notNull)) + "values" + \
               str(tuple(["insertValue" for i in range(0, len(notNull))])) + ";"
         res = self.executor(sql)
